6972b49518bcf99d56076a9a0ada81f4863ba953
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-04-14 17:25:20 +0900

    DotnetNote 게시판 및 단위테스트 커밋

---
+++ DotNetNote/Helpers.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helpers.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helpers.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec3f9e21-8e1e-45b2-b314-f99ebbfb5163")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNetNote/Helpers.Test/StringLibraryTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Helpers.Test
+{
+    [TestClass]
+    public class StringLibraryTest
+    {
+        [TestMethod]
+        public void CutStringTest()
+        {
+            string strCut = "Hello World, This is a test sentence";
+            int intChar = 15;
+
+            var expected = "Hello World,..."; // CutString 예상 결과
+            var actual = StringLibrary.CutString(strCut, intChar);
+            // Assert 클래스
+            // : 단위 테스트 내에서 다양한 조건을 테스트하기 위한 도우미 클래스의 컬렉션입니다. 테스트 중인 조건이 충족되지 않으면 예외가 throw됩니다.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CutStringUnicodeTest()
+        {
+            string strCut = "안녕하세요. 부경대학교입니다.";
+            int intChar = 9;
+
+            var expected = "안녕하세요....";
+            var actual = StringLibrary.CutStringUnicode(strCut, intChar);
+            // Assert 클래스
+            // : 단위 테스트 내에서 다양한 조건을 테스트하기 위한 도우미 클래스의 컬렉션입니다. 테스트 중인 조건이 충족되지 않으면 예외가 throw됩니다.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var expected = 10;
+            var actual = (5 + 5);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
