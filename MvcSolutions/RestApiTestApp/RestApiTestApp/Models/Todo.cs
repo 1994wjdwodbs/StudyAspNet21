cd547e9def34432398d4d23b82efd348ef5ea171
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-04-20 14:29:47 +0900

    포트폴리오 ASP.NET CORE MVC 커밋

---
+++ MvcSolutions/RestApiTestApp/RestApiTestApp/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiTestApp.Models
+{
+    public class Todo
+    {
+        [Key] // 기본 키
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "제목은 필수입니다.")] // NOT NULL
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
